0dcae9a37659f249d7f83406dc181ed6ca4db936
Uri Shaked <uri@urishaked.com>
2025-09-18 21:09:12 +0300

    rename to tt_um_librelane3_test_rename

--- src/tt_um_librelane3_test.v
+++ src/tt_um_librelane3_test.v
@@ -8,7 +8,7 @@
 
 `default_nettype none
 
-module tt_um_librelane3_test1 (
+module tt_um_librelane3_test_rename (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
